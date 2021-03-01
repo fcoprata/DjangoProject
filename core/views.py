c8f8580c18536aeb5da54c4f390dc4732dbf4311
fcoprata <fcoprata@alu.ufc.br>
2021-02-28 23:01:05 -0300

    atualização

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def hello(requests,nome,idade):
-    return HttpResponse(f'<h1>Hello {nome}  de {idade}</h1>')
+    return HttpResponse(f'<h1>Hello {nome}  de {idade} anos</h1>')
 def soma(requests,n1,n2):
     return HttpResponse(f'soma de {n1}+{n2}={n1+n2}')
 def sub(requests,n1,n2):
